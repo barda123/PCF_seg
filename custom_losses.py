00d227323cefd813e6414682d084e850da3c9b0d
Andrew Bard <andybard@googlemail.com>
2020-03-10 15:31:35 +0000

    updated unet_dev and custom_losses so that the exponentially-smoothed loss function weights ACTUALLY WORK. phew.

--- custom_losses.py
+++ custom_losses.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from scipy.ndimage import morphology
 
-from tensorflow as tf
+import tensorflow as tf
 from tensorflow.keras import backend as keras
 
 
@@ -32,3 +32,84 @@ def binary_crossentropy_weight_balance(yTrue,yPred):
     wcce = bce * weights
     
     return tf.reduce_mean(wcce)
+
+
+
+def binary_crossentropy_weight_dict(weightDict):
+
+    '''returns a  function for calculating weights, but DOESN'T WORK YET '''    
+    
+    def weight_from_dict(yTrue,weightDict):
+    
+        '''returns the pixelwise weights based on the dict'''
+
+        weights = tf.zeros_like(yTrue,dtype='float64')
+
+        for yVal in weightDict.keys():
+
+            weights += tf.cast(tf.math.equal(yTrue,yVal),'float64')*weightDict[yVal]
+
+        return yTrue
+
+    
+    def loss(yTrue,yPred):
+    
+        weights = weight_from_dict(yTrue,weightDict)
+    
+        bce = keras.binary_crossentropy(yPred, yTrue)
+    
+        wcce = bce * weights
+        
+        return tf.reduce_mean(wcce)
+        
+    return loss
+
+
+
+
+def closeness_to_foreground_balanced(mask,sigma=20):
+    
+    '''takes a boolean image (mask) and computes a weight map for it, such that the classes are balanced, and there is an exponential smoothing moving away from the forground pixels.'''
+    
+    distance = morphology.distance_transform_edt(1-mask)
+    
+    closeness = np.exp(-distance/sigma) #which will be between 0 and 1
+    
+    #get class imbalance, assuming that there are less +ve pixels
+    imbalance = tf.cast(tf.size(mask),tf.float32)/tf.reduce_sum(mask)
+    
+    #rescale to be between 1 and imbalance: i.e. the fg pixels are imbalance, the far bg are ~1, and other pixels are between these 2 values.
+    closeness = closeness*(imbalance-1)+1
+    
+    #rescale for mean of 1
+    closeness /= np.mean(closeness)
+    
+    return closeness
+
+def closeness_to_foreground_balanced_tensor(mask,sigma=20):
+
+    '''applies closeness_to_foreground_balanced to a rank-3 or rank-4 mask array (i.e. examples of images stacked along axis 0)'''
+    
+    closeness = tf.map_fn(lambda x: closeness_to_foreground_balanced(x,sigma),mask)
+    
+    return closeness
+
+
+def binary_crossentropy_closeness_to_foreground(sigma=20):
+    
+    '''returns a loss function which weights pixels according to an exponentially-smoothed class imbalance. Spatial smoothing parameter sigma must be input'''
+    
+    def lossFunction(yTrue,yPred):
+    
+        #apply the closeness function to each image within the True mask, and re-concatenate
+        weights = tf.py_function(func=closeness_to_foreground_balanced_tensor,
+                                 inp=[yTrue,sigma],
+                                 Tout=tf.float32)
+        
+        bce = keras.binary_crossentropy(yPred, yTrue)
+    
+        wcce = tf.multiply(bce, weights)
+        
+        return tf.reduce_mean(wcce)
+    
+    return lossFunction
